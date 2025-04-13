36415e87cd4d4d962772e87cf39f600c83fb8feb
kyutekrap <katepark1225@naver.com>
2025-04-14 06:29:27 +0900

    v0.2.0

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "./transpiler/transpiler.h"
 
-int main() {
-    transpiler("C:\\Users\\user\\Link\\_example\\main.link");
+int main(int argc, char *argv[]) {
+    if (argc < 2)
+    {
+        transpiler("C:\\Users\\user\\Link\\_example\\main.link");
+    }
+    else
+    {
+        transpiler(argv[1]);
+    }
+
     return 0;
 }
